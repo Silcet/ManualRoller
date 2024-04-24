25847d42c5dbd88fb16837d42df4b421531f7e64
Silcet <camorga1@gmail.com>
2024-04-24 23:14:48 +0200

    Random attempt at fixing async function

--- scripts/manualroller.mjs
+++ scripts/manualroller.mjs
@@ -1,28 +1,7 @@
 import ManualRollerResolver from "./lib/manual-roller-resolver.js";
-import FACES from "./lib/manual-roller-resolver.js";
 import DiceConfig from "./lib/manual-roller-config.mjs";
 
-Hooks.once('init', () => {
-    // FACES.forEach((item, _) => {
-    //     game.settings.register("manual-roller", "d" + item + "roll", {
-    //         name: "ManualRoller d" + item + "value",
-    //         hint: "The value to roll everytime you roll a d" + item,
-    //         scope: "client",
-    //         config: true,
-    //         requiresReload: false,
-    //         type: Number,
-    //         range: {
-    //             min: 1,
-    //             max: item,
-    //             step: 1
-    //         },
-    //         default: 1,
-    //         onChange: value => {
-    //             console.log("Dice " + item);
-    //             console.log(value);
-    //         }
-    //     });
-    // });
+Hooks.once('init', async function() {
     game.settings.register("manual-roller", "diceSettings", {
         config: false,
         default: {},
@@ -46,7 +25,7 @@ Hooks.once('init', () => {
         { id: "d10", faces: 10, icon: "fa-dice-d10" },
         { id: "d12", faces: 12, icon: "fa-dice-d12" },
         { id: "d20", faces: 20, icon: "fa-dice-d20" },
-        { id: "d100", faces: 100, icon: "fa-dice-d100" },
+        { id: "d100", faces: 100, icon: "fa-dice-d100" }
     ];
 });
 
